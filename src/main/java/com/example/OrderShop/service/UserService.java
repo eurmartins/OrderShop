a1de75152f7a78c270119a755397a05895214d5e
eurmartins <vitaocap123@gmail.com>
2025-03-15 15:20:03 -0300

    Criando o Usuario Service

---
+++ src/main/java/com/example/OrderShop/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.OrderShop.service;
+
+import com.example.OrderShop.dto.UsuarioRequestDTO;
+import com.example.OrderShop.dto.UsuarioResponseDTO;
+import com.example.OrderShop.mapper.UsuarioMapper;
+import com.example.OrderShop.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioMapper usuarioMapper;
+
+    public UsuarioResponseDTO saveUsuario(UsuarioRequestDTO request){
+        return usuarioMapper.toDto(usuarioRepository.save(usuarioMapper.toEntity(request)));
+    }
+
+    public List<UsuarioResponseDTO> listAllUsuarios(){
+        return usuarioMapper.toListToResponseDto(usuarioRepository.findAll());
+    }
+
+    public void deleteUsuario(Long id){
+        usuarioRepository.deleteById(id);
+    }
+}
